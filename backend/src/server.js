f4597da8528440340d50cd5d9e3997a7dc2a7080
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-02 19:58:44 -0300

    Added cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 mongoose.connect('mongodb://192.168.99.100:27017/aircnc', {
@@ -8,6 +9,7 @@ mongoose.connect('mongodb://192.168.99.100:27017/aircnc', {
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
