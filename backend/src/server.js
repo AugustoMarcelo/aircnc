6a8afbead6ab693f89aa0822574b32b3f1ec4e31
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-02 20:58:50 -0300

    Added a static route to show files

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes');
 const cors = require('cors');
+const path = require('path');
+
+const routes = require('./routes');
 
 const app = express();
 mongoose.connect('mongodb://192.168.99.100:27017/aircnc', {
@@ -11,6 +13,7 @@ mongoose.connect('mongodb://192.168.99.100:27017/aircnc', {
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
