e3f7d088729d7d81564e4ab363f5e76079590be2
AugustoMarcelo <mrclgst10@gmail.com>
2019-10-06 16:00:29 -0300

    Setting web socket protocol

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,12 +3,33 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
 mongoose.connect('mongodb://192.168.99.100:27017/aircnc', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+});
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((request, response, next) => {
+  request.io = io;
+  request.connectedUsers = connectedUsers;
+  
+  return next();
 });
 
 app.use(cors());
@@ -17,4 +38,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 
-app.listen(3333);
+server.listen(3333);
